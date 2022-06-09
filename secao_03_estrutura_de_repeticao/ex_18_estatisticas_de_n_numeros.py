bad189216ae4731315ac7e8657e02b607a3b7402
Bruna Andrade <brunansandrade@gmail.com>
2022-06-08 22:50:40 -0300

    ex18 s03, ainda nao passa no teste de lista vazia

--- secao_03_estrutura_de_repeticao/ex_18_estatisticas_de_n_numeros.py
+++ secao_03_estrutura_de_repeticao/ex_18_estatisticas_de_n_numeros.py
@@ -18,3 +18,11 @@ Faça um programa que, dado um conjunto de N números, determine o menor valor, 
 
 def calcular_estatisticas(*numeros) -> str:
     """Escreva aqui em baixo a sua solução"""
+    maior = max(numeros)
+    menor = min(numeros)
+    soma = sum(numeros)
+
+    if len(numeros) == '':
+        print("'Maior valor: não existe. Menor valor: não existe. Soma: 0'")
+    else:
+        print(f"'Maior valor: {maior}. Menor valor: {menor}. Soma: {soma}'")
