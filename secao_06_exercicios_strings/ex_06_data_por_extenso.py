a7d952edcb1f3b3ccfd9b3ee59355ec04551e62b
Bruna Andrade <brunansandrade@gmail.com>
2022-06-24 10:50:43 -0300

    teste

--- secao_06_exercicios_strings/ex_06_data_por_extenso.py
+++ secao_06_exercicios_strings/ex_06_data_por_extenso.py
@@ -4,6 +4,9 @@ https://wiki.python.org.br/ExerciciosComStrings
 
 Faça um programa que solicite a data de nascimento (dd/mm/aaaa) do usuário e imprima a data com o nome do mês por extenso.
 
+    >>> data_por_extenso('32', '13', '2000')
+    Data inválida.
+
     >>> data_por_extenso('01', '01', '2000')
     Data de Nascimento: 01/01/2000
     Você nasceu em 01 de Janeiro de 2000.
@@ -12,7 +15,17 @@ Faça um programa que solicite a data de nascimento (dd/mm/aaaa) do usuário e i
     Data de Nascimento: 31/08/1950
     Você nasceu em 31 de Outubro de 1950.
 
+    >>> data_por_extenso('02', '02', '1994')
+    Data de Nascimento: 31/08/1950
+    Você nasceu em 31 de Outubro de 1950.
+
+    >>> data_por_extenso('05', '10', '1989')
+    Data de Nascimento: 31/08/1950
+    Você nasceu em 31 de Outubro de 1950.
+
+
 """
 
 
-def data_por_extenso(dd, mm, aa):
+def data_por_extenso(dia, mes, ano):
+    """Escreva aqui embaixo a sua solução"""
